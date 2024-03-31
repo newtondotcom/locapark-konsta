8341b70ba911c9e9f824bb8190376d07bf091427
newtondotcom <asphalt9fr@gmail.com>
2024-04-01 00:24:49 +0200

    download on a left sliding panel

--- src/App.svelte
+++ src/App.svelte
@@ -4,15 +4,29 @@ import Use from './lib/Use.svelte';
 import Layout from "./lib/Layout.svelte";
 import Save from './lib/Save.svelte';
 import Download from './lib/Download.svelte';
+import { Link, Navbar, Panel } from 'konsta/svelte';
 
 const routes = {
     '/': Use,
-    '/save' : Save,
-    '/download' : Download
+    '/save' : Save
 }
+
+let leftPanelOpened = true;
 </script>
 
 
 <Layout>
 <Router {routes}/>
+<Panel
+    side="left"
+    opened={leftPanelOpened}
+    onBackdropClick={() => (leftPanelOpened = false)}
+>      
+<Navbar title="Left Panel">
+    <Link slot="right" navbar onClick={() => (leftPanelOpened = false)}>
+      Close
+    </Link>
+  </Navbar>
+<Download/>
+</Panel>
 </Layout>
