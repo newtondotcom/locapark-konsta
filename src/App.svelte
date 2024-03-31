584f667de3a4c5dd0ed38a8695dc0791050b7ed7
newtondotcom <asphalt9fr@gmail.com>
2024-04-01 01:27:46 +0200

    minor edits

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,13 @@ import Router from 'svelte-spa-router';
 import Use from './lib/Use.svelte';
 import Layout from "./lib/Layout.svelte";
 import Save from './lib/Save.svelte';
-import Download from './lib/Download.svelte';
+import Display from './lib/Display.svelte';
 import { Link, Navbar, Panel } from 'konsta/svelte';
 
 const routes = {
     '/': Use,
-    '/save' : Save
+    '/save' : Save,
+    '/display' : Display
 }
 </script>
 
