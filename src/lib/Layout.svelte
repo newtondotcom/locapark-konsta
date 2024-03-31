85716f5bc3304ba503cf4236e9b3baae2edee098
newtondotcom <asphalt9fr@gmail.com>
2024-04-01 00:27:45 +0200

    factor

--- src/lib/Layout.svelte
+++ src/lib/Layout.svelte
@@ -1,26 +1,26 @@
 <script>
-    // @ts-ignore
-    import { App, Page, Navbar, Panel, Link} from 'konsta/svelte';
-    let leftPanelOpened = true;
-    import Download from './Download.svelte';
-  </script>
-  
-  <App theme="ios">
-      <Page>
-        <Navbar title="Locapark" />
+  // @ts-ignore
+  import { App, Page, Navbar, Panel, Link } from "konsta/svelte";
+  let leftPanelOpened = true;
+  import Download from "./Download.svelte";
+</script>
 
-<Panel
-side="left"
-opened={leftPanelOpened}
-onBackdropClick={() => (leftPanelOpened = false)}
->      
-<Navbar title="Left Panel">
-<Link slot="right" navbar onClick={() => (leftPanelOpened = false)}>
-  Close
-</Link>
-</Navbar>
-<Download/>
-</Panel>
-        <slot></slot>
-      </Page>
-  </App>
+<App theme="ios">
+  <Page>
+    <Navbar title="Locapark" />
+
+    <Panel
+      side="left"
+      opened={leftPanelOpened}
+      onBackdropClick={() => (leftPanelOpened = false)}
+    >
+      <Navbar title="Left Panel">
+        <Link slot="right" navbar onClick={() => (leftPanelOpened = false)}>
+          Close
+        </Link>
+      </Navbar>
+      <Download />
+    </Panel>
+    <slot />
+  </Page>
+</App>
